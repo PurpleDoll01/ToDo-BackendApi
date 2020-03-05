c2249e10400686fe002a20d125432aec29fd59aa
Mariana Valencia <purple.mv@gmail.com>
2020-03-05 18:22:44 -0500

    Sirviendo archivos estaticos

--- server.js
+++ server.js
@@ -30,5 +30,8 @@ router.post('/message', function (req, res) {
     }   
 });
 
+app.use('/app', express.static('public'));
+//Aquí se están sirviendo estáticos HTML 
+
 app.listen(3000);
 console.log('La app está escuchando en http://localhost:3000');
