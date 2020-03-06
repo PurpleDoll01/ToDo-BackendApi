b198e8dee82adca367ab32e517a587ba1f80e560
Mariana Valencia <purple.mv@gmail.com>
2020-03-06 16:20:05 -0500

    Conectando a Mongo Atlas, leyendo y escribiendo

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,27 @@
+const db = require('mongoose');
+const Model = require('./model');
+
 const list = [];
 
+//mongodb+srv://PurpleDoll:Thestral01@messagecluster-vslg9.mongodb.net/test
+db.Promise = global.Promise;
+db.connect('mongodb+srv://PurpleDoll:Thestral01@messagecluster-vslg9.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'db_message'
+});
+db.connection.on('connected', () => {
+    console.log('Mongoose is connecteeed');
+}) 
+
 function addMessage(message) {
-    list.push(message);
+    const myMessage = new Model(message);
+    myMessage.save();
 }
 
-function getMessages() {
-    return list;
+async function getMessages() {
+    const messages = await Model.find();
+    return messages;
 }
 
 module.exports = {
